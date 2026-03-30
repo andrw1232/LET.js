5fa15d61fed50b4ba925a3e79c2b6586368ed76d
Andrew <andrew.traynor.stu@kingsu.ca>
2026-03-30 15:11:48 -0600

    working locally hosted web server

--- Let/Server.js
+++ Let/Server.js
@@ -1,15 +1,49 @@
-import http from 'node:http';
+import Interpreter from './Interpreter.js';
+
+import express from 'express';
+
+
 
 const hostname = '127.0.0.1';
 const port = 8080;
-const server = http.createServer((req, res) => {
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.set('views','./views');
+app.set('view engine', 'pug');
+
+// makes the form data available in req.body
+app.use(express.urlencoded({extended: true }));
+
+
+// handle the initial get request
+app.get("/", (req,res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html><body><h1>hello world</h1></body></html>');
-    res.end();
+    res.setHeader('Content-Type', 'text/html');    
     
+    res.render('index', {body: "", answer: ""});
 });
 
-server.listen(port, hostname, () => {
+
+// handle the post request that involve parsing 
+app.post("/" , (req,res) => {
+
+    var result;
+    try {
+        result = Interpreter.parse(req.body.in);
+    } catch (error) {
+        result = "An Error was encountered: "+error.message
+    }
+    
+    res.statusCode = 200;
+    res.setHeader('Content-Type', "text/html");
+
+    res.render('index', {body: req.body.in, answer: result}); // render the site with the answer
+});
+
+
+app.listen(port, hostname, () => {
     console.log(`server is running at http://${hostname}:${port}`);
 });
